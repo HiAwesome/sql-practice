418354fad5b12d5f866a003574538191f15c04c3
moqi <moqimoqidea@gmail.com>
2020-07-23 21:34:46 +0800

    20200723

--- hive/lateral_view.sql
+++ hive/lateral_view.sql
@@ -1,5 +1,3 @@
-show tables;
-
 select
     age
 from (
@@ -8,7 +6,3 @@ from (
 ) t
 LATERAL VIEW json_tuple(data1, 'info') t1 as info
 LATERAL VIEW json_tuple(info, 'age') t2 as age;
-
-select * from explode_array_test;
-
-show create  table explode_array_test;
