8f409fe8c30cb38a62c3585c4ead112e6f146517
moqi <moqimoqidea@gmail.com>
2020-07-27 10:16:01 +0800

    20200727

---
+++ mysql/sql10minutes/chapter03.sql
@@ -0,0 +1,21 @@
+# 按列位置排序
+select
+    prod_id,
+    prod_price,
+    prod_name
+from
+    Products
+order by
+    2, 3;
+/*
+BNBG02	3.49	Bird bean bag toy
+BNBG01	3.49	Fish bean bag toy
+BNBG03	3.49	Rabbit bean bag toy
+RGAN01	4.99	Raggedy Ann
+BR01	5.99	8 inch teddy bear
+BR02	8.99	12 inch teddy bear
+RYL01	9.49	King doll
+RYL02	9.49	Queen doll
+BR03	11.99	18 inch teddy bear
+*/
+
