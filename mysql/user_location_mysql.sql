f6c85fe1a1c845d8eaf653049d9f9aa7ebe26faa
moqi <moqimoqidea@gmail.com>
2020-07-29 17:21:22 +0800

    20200729

--- mysql/user_location_mysql.sql
+++ mysql/user_location_mysql.sql
@@ -35,17 +35,71 @@ CREATE TABLE IF NOT EXISTS user_location
 -- 插入数据
 insert into
     user_location
-select 'user_a', 'location_a', '2018-01-01 08:00:00', 60
-UNION ALL select 'user_a', 'location_a', '2018-01-01 09:00:00', 60
-UNION ALL select 'user_a', 'location_a', '2018-01-01 11:00:00', 60
-UNION ALL select 'user_a', 'location_a', '2018-01-01 12:00:00', 60
-UNION ALL select 'user_a', 'location_b', '2018-01-01 10:00:00', 60
-UNION ALL select 'user_a', 'location_c', '2018-01-01 08:00:00', 60
-UNION ALL select 'user_a', 'location_c', '2018-01-01 09:00:00', 60
-UNION ALL select 'user_a', 'location_c', '2018-01-01 10:00:00', 60
-UNION ALL select 'user_b', 'location_a', '2018-01-01 15:00:00', 60
-UNION ALL select 'user_b', 'location_a', '2018-01-01 16:00:00', 60
-UNION ALL select 'user_b', 'location_a', '2018-01-01 18:00:00', 60;
+select
+    'user_a',
+    'location_a',
+    '2018-01-01 08:00:00',
+    60
+UNION ALL
+select
+    'user_a',
+    'location_a',
+    '2018-01-01 09:00:00',
+    60
+UNION ALL
+select
+    'user_a',
+    'location_a',
+    '2018-01-01 11:00:00',
+    60
+UNION ALL
+select
+    'user_a',
+    'location_a',
+    '2018-01-01 12:00:00',
+    60
+UNION ALL
+select
+    'user_a',
+    'location_b',
+    '2018-01-01 10:00:00',
+    60
+UNION ALL
+select
+    'user_a',
+    'location_c',
+    '2018-01-01 08:00:00',
+    60
+UNION ALL
+select
+    'user_a',
+    'location_c',
+    '2018-01-01 09:00:00',
+    60
+UNION ALL
+select
+    'user_a',
+    'location_c',
+    '2018-01-01 10:00:00',
+    60
+UNION ALL
+select
+    'user_b',
+    'location_a',
+    '2018-01-01 15:00:00',
+    60
+UNION ALL
+select
+    'user_b',
+    'location_a',
+    '2018-01-01 16:00:00',
+    60
+UNION ALL
+select
+    'user_b',
+    'location_a',
+    '2018-01-01 18:00:00',
+    60;
 
 select *
 from
@@ -56,30 +110,32 @@ select
     user_id,
     location_id,
     from_unixtime(start_time) as u_dt,
-    max(r1) * 60 as duration
-from (
-    select
-        user_id,
-        location_id,
-        dt - (r1 * 3600 - duration * 60) as start_time,
-        r1
-    from (
-        SELECT
+    max(r1) * 60              as duration
+from
+    (
+        select
             user_id,
             location_id,
-            u_dt,
-            UNIX_TIMESTAMP(u_dt) AS                         dt,
-            duration,
-            row_number() over w  as                         r1,
-            hour(u_dt) - hour(first_value(u_dt) OVER w) + 1 gap_hour_plus_one
-        FROM
-            user_location
-            window
-                w as (PARTITION BY user_id, location_id ORDER BY u_dt)
-    ) t
-    where
-        r1 = gap_hour_plus_one
-) t1
+            dt - (r1 * 3600 - duration * 60) as start_time,
+            r1
+        from
+            (
+                SELECT
+                    user_id,
+                    location_id,
+                    u_dt,
+                    UNIX_TIMESTAMP(u_dt) AS                         dt,
+                    duration,
+                    row_number() over w  as                         r1,
+                    hour(u_dt) - hour(first_value(u_dt) OVER w) + 1 gap_hour_plus_one
+                FROM
+                    user_location
+                    window
+                        w as (PARTITION BY user_id, location_id ORDER BY u_dt)
+            ) t
+        where
+            r1 = gap_hour_plus_one
+    ) t1
 group by
     user_id,
     location_id,
@@ -96,44 +152,47 @@ select
     user_id,
     location_id,
     from_unixtime(start_time) as u_dt,
-    max(new_r1) * 60 as duration
-from (
-    select
-        user_id,
-        location_id,
-        dt - (new_r1 * 3600 - duration * 60) as start_time,
-        new_r1
-    from (
+    max(new_r1) * 60          as duration
+from
+    (
         select
             user_id,
             location_id,
-            u_dt,
-            dt,
-            duration,
-            row_number() over w  as                         new_r1,
-            hour(u_dt) - hour(first_value(u_dt) OVER w) + 1 new_gap_hour_plus_one
-        from (
-            SELECT
-                user_id,
-                location_id,
-                u_dt,
-                UNIX_TIMESTAMP(u_dt) AS                         dt,
-                duration,
-                row_number() over w  as                         r1,
-                hour(u_dt) - hour(first_value(u_dt) OVER w) + 1 gap_hour_plus_one
-            FROM
-                user_location
-                window
-                    w as (PARTITION BY user_id, location_id ORDER BY u_dt)
-        ) t
+            dt - (new_r1 * 3600 - duration * 60) as start_time,
+            new_r1
+        from
+            (
+                select
+                    user_id,
+                    location_id,
+                    u_dt,
+                    dt,
+                    duration,
+                    row_number() over w as                          new_r1,
+                    hour(u_dt) - hour(first_value(u_dt) OVER w) + 1 new_gap_hour_plus_one
+                from
+                    (
+                        SELECT
+                            user_id,
+                            location_id,
+                            u_dt,
+                            UNIX_TIMESTAMP(u_dt) AS                         dt,
+                            duration,
+                            row_number() over w  as                         r1,
+                            hour(u_dt) - hour(first_value(u_dt) OVER w) + 1 gap_hour_plus_one
+                        FROM
+                            user_location
+                            window
+                                w as (PARTITION BY user_id, location_id ORDER BY u_dt)
+                    ) t
+                where
+                    r1 != gap_hour_plus_one
+                    window
+                        w as (PARTITION BY user_id, location_id ORDER BY u_dt)
+            ) t1
         where
-            r1 != gap_hour_plus_one
-        window
-            w as (PARTITION BY user_id, location_id ORDER BY u_dt)
-    ) t1
-    where
-        new_r1 = new_gap_hour_plus_one
-) t2
+            new_r1 = new_gap_hour_plus_one
+    ) t2
 group by
     user_id,
     location_id,
@@ -148,30 +207,32 @@ select
     user_id,
     location_id,
     from_unixtime(start_time) as u_dt,
-    max(r1) * 60 as duration
-from (
-    select
-        user_id,
-        location_id,
-        dt - (r1 * 3600 - duration * 60) as start_time,
-        r1
-    from (
-        SELECT
+    max(r1) * 60              as duration
+from
+    (
+        select
             user_id,
             location_id,
-            u_dt,
-            UNIX_TIMESTAMP(u_dt) AS                         dt,
-            duration,
-            row_number() over w  as                         r1,
-            hour(u_dt) - hour(first_value(u_dt) OVER w) + 1 gap_hour_plus_one
-        FROM
-            user_location
-            window
-                w as (PARTITION BY user_id, location_id ORDER BY u_dt)
-    ) t
-    where
-        r1 = gap_hour_plus_one
-) t1
+            dt - (r1 * 3600 - duration * 60) as start_time,
+            r1
+        from
+            (
+                SELECT
+                    user_id,
+                    location_id,
+                    u_dt,
+                    UNIX_TIMESTAMP(u_dt) AS                         dt,
+                    duration,
+                    row_number() over w  as                         r1,
+                    hour(u_dt) - hour(first_value(u_dt) OVER w) + 1 gap_hour_plus_one
+                FROM
+                    user_location
+                    window
+                        w as (PARTITION BY user_id, location_id ORDER BY u_dt)
+            ) t
+        where
+            r1 = gap_hour_plus_one
+    ) t1
 group by
     user_id,
     location_id,
@@ -181,44 +242,47 @@ select
     user_id,
     location_id,
     from_unixtime(start_time) as u_dt,
-    max(new_r1) * 60 as duration
-from (
-    select
-        user_id,
-        location_id,
-        dt - (new_r1 * 3600 - duration * 60) as start_time,
-        new_r1
-    from (
+    max(new_r1) * 60          as duration
+from
+    (
         select
             user_id,
             location_id,
-            u_dt,
-            dt,
-            duration,
-            row_number() over w  as                         new_r1,
-            hour(u_dt) - hour(first_value(u_dt) OVER w) + 1 new_gap_hour_plus_one
-        from (
-            SELECT
-                user_id,
-                location_id,
-                u_dt,
-                UNIX_TIMESTAMP(u_dt) AS                         dt,
-                duration,
-                row_number() over w  as                         r1,
-                hour(u_dt) - hour(first_value(u_dt) OVER w) + 1 gap_hour_plus_one
-            FROM
-                user_location
-                window
-                    w as (PARTITION BY user_id, location_id ORDER BY u_dt)
-        ) t
+            dt - (new_r1 * 3600 - duration * 60) as start_time,
+            new_r1
+        from
+            (
+                select
+                    user_id,
+                    location_id,
+                    u_dt,
+                    dt,
+                    duration,
+                    row_number() over w as                          new_r1,
+                    hour(u_dt) - hour(first_value(u_dt) OVER w) + 1 new_gap_hour_plus_one
+                from
+                    (
+                        SELECT
+                            user_id,
+                            location_id,
+                            u_dt,
+                            UNIX_TIMESTAMP(u_dt) AS                         dt,
+                            duration,
+                            row_number() over w  as                         r1,
+                            hour(u_dt) - hour(first_value(u_dt) OVER w) + 1 gap_hour_plus_one
+                        FROM
+                            user_location
+                            window
+                                w as (PARTITION BY user_id, location_id ORDER BY u_dt)
+                    ) t
+                where
+                    r1 != gap_hour_plus_one
+                    window
+                        w as (PARTITION BY user_id, location_id ORDER BY u_dt)
+            ) t1
         where
-            r1 != gap_hour_plus_one
-        window
-            w as (PARTITION BY user_id, location_id ORDER BY u_dt)
-    ) t1
-    where
-        new_r1 = new_gap_hour_plus_one
-) t2
+            new_r1 = new_gap_hour_plus_one
+    ) t2
 group by
     user_id,
     location_id,
@@ -227,6 +291,79 @@ order by
     user_id,
     location_id,
     u_dt;
+/*
+user_a	location_a	2018-01-01 08:00:00	120
+user_a	location_a	2018-01-01 11:00:00	120
+user_a	location_b	2018-01-01 10:00:00	60
+user_a	location_c	2018-01-01 08:00:00	180
+user_b	location_a	2018-01-01 15:00:00	120
+user_b	location_a	2018-01-01 18:00:00	60
+*/
+
+
+select
+    user_id,
+    location_id,
+    u_dt,
+    duration,
+    first_value(u_dt) over w as                                                   fv,
+    row_number() over w      as                                                   rn,
+    TIMESTAMPDIFF(hour, (first_value(u_dt) over w), u_dt)                         dd,
+    (row_number() over w) - TIMESTAMPDIFF(hour, (first_value(u_dt) over w), u_dt) gap
+from
+    user_location
+    window
+        w as (partition by user_id, location_id order by u_dt);
+/*
+user_a	location_a	2018-01-01 08:00:00	60	2018-01-01 08:00:00	1	0	1
+user_a	location_a	2018-01-01 09:00:00	60	2018-01-01 08:00:00	2	1	1
+user_a	location_a	2018-01-01 11:00:00	60	2018-01-01 08:00:00	3	3	0
+user_a	location_a	2018-01-01 12:00:00	60	2018-01-01 08:00:00	4	4	0
+user_a	location_b	2018-01-01 10:00:00	60	2018-01-01 10:00:00	1	0	1
+user_a	location_c	2018-01-01 08:00:00	60	2018-01-01 08:00:00	1	0	1
+user_a	location_c	2018-01-01 09:00:00	60	2018-01-01 08:00:00	2	1	1
+user_a	location_c	2018-01-01 10:00:00	60	2018-01-01 08:00:00	3	2	1
+user_b	location_a	2018-01-01 15:00:00	60	2018-01-01 15:00:00	1	0	1
+user_b	location_a	2018-01-01 16:00:00	60	2018-01-01 15:00:00	2	1	1
+user_b	location_a	2018-01-01 18:00:00	60	2018-01-01 15:00:00	3	3	0
+*/
+
+select
+    user_id,
+    location_id,
+    start_time,
+    max(rn1 * 60) as duration
+from
+    (
+        select
+            user_id,
+            location_id,
+            u_dt,
+            row_number() over w1      as rn1,
+            first_value(u_dt) over w1 as start_time
+        from
+            (
+                select
+                    user_id,
+                    location_id,
+                    u_dt,
+                    (row_number() over w) - TIMESTAMPDIFF(hour, (first_value(u_dt) over w), u_dt) gap
+                from
+                    user_location
+                    window
+                        w as (partition by user_id, location_id order by u_dt)
+            ) t1
+            window
+                w1 as (partition by user_id, location_id, gap order by u_dt)
+    ) t2
+group by
+    user_id,
+    location_id,
+    start_time
+order by
+    user_id,
+    location_id,
+    start_time;
 /*
 user_a	location_a	2018-01-01 08:00:00	120
 user_a	location_a	2018-01-01 11:00:00	120
